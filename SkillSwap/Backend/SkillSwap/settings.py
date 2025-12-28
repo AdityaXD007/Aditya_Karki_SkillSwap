9e5cb8a21b3a8ae8333463145777013d87b61e25
Aditya Karki <karkikaaditya12345@gmail.com>
2025-12-28 22:08:00 +0545

    Connected the database to postgres sql and migrated all the data to skillswap database in postgres

--- SkillSwap/Backend/SkillSwap/settings.py
+++ SkillSwap/Backend/SkillSwap/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'SkillSwap.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "skillswap",
+        "USER": "aditya",
+        "PASSWORD": "aditya",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
